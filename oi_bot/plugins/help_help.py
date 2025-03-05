a7e83dc69eaf0a3c6507933cb9791708cb8c3125
talentestors <talentestors@gmail.com>
2025-03-05 15:01:46 +0800

    ✅ Add help test

--- oi_bot/plugins/help_help.py
+++ oi_bot/plugins/help_help.py
@@ -1,20 +1,17 @@
-# show_menu.py
+# help_help.py
 # Author: talentestors
 # Created on 2025-03-05
 
+from nonebot import on_command
+from nonebot.rule import to_me
+
 # define the help menu
-# - 今日比赛: 获取今日比赛信息
-# - 明日比赛: 获取明日比赛信息
-# - 每日一题: 获取 LeetCode 每日一题
-# - 洛谷日报: 获取洛谷日报
-# - luoguluck: 获取洛谷运势
-# - help: 获取帮助信息
 HELP_LIST = """HELP MENU:
 1. 今日比赛: 获取今日比赛信息
 2. 明日比赛: 获取明日比赛信息
 3. 每日一题: 获取 LeetCode 每日一题
 4. 洛谷日报: 获取洛谷日报
-5. luoguluck: 获取洛谷运势
+5. luck: 获取今日运势
 6. help: 获取帮助信息"""
 
 
@@ -30,7 +27,15 @@ def get_show_menu() -> str:
     2. 明日比赛: 获取明日比赛信息
     3. 每日一题: 获取 LeetCode 每日一题
     4. 洛谷日报: 获取洛谷日报
-    5. luoguluck: 获取洛谷运势
+    5. luck: 获取今日运势
     6. help: 获取帮助信息
     """
     return HELP_LIST
+
+
+help = on_command("help", rule=to_me(), priority=1, block=True)
+
+
+@help.handle()
+async def handle_help(bot, event):
+    await help.finish(get_show_menu())
